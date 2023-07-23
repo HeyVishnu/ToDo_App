94e52813fed3f766f0d29ea8d43df72fbe29e99a
HeyVishnu <108324467+HeyVishnu@users.noreply.github.com>
2023-07-23 19:51:32 +0530

    Update connect.js

--- data/connect.js
+++ data/connect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 
 export const connect = () =>{
-    mongoose.connect("mongodb://127.0.0.1:27017", {dbName: "backendapi"})
+    mongoose.connect(MONGO_URI, {dbName: "backendapi"})
     .then(()=>{
         console.log("backend connected")
     })
